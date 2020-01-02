862b172f2b7b59a55a26283409b0739b11629e2e
Mingun <alexander_sergey@mail.ru>
2020-01-02 17:07:01 +0500

    Merge `window_create` into `window_init`

--- xneur/lib/main/window.c
+++ xneur/lib/main/window.c
@@ -67,7 +67,17 @@ static int is_has_net_supported(Display *display, Window window)
 	return result;
 }
 
-static int window_create(struct _window *p)
+static void window_uninit(struct _window *p)
+{
+	if (p->keymap != NULL)
+		p->keymap->uninit(p->keymap);
+
+	free(p);
+
+	log_message(DEBUG, _("Window is freed"));
+}
+
+struct _window* window_init(struct _xneur_handle *handle)
 {
 	XSetErrorHandler(error_handler);
 
@@ -75,7 +85,7 @@ static int window_create(struct _window *p)
 	if (!display)
 	{
 		log_message(ERROR, _("Can't connect to XServer"));
-		return FALSE;
+		return NULL;
 	}
 
 	Window root = DefaultRootWindow(display);
@@ -85,7 +95,7 @@ static int window_create(struct _window *p)
 	{
 		log_message(ERROR, _("Can't create program window"));
 		XCloseDisplay(display);
-		return FALSE;
+		return NULL;
 	}
 
 	// Create flag window
@@ -97,7 +107,7 @@ static int window_create(struct _window *p)
 	{
 		log_message(ERROR, _("Can't create flag window"));
 		XCloseDisplay(display);
-		return FALSE;
+		return NULL;
 	}
 
 	// Set no border mode to flag window
@@ -119,42 +129,27 @@ static int window_create(struct _window *p)
 
 	XChangeProperty(display, flag_window, win_prop, win_prop, 32, PropModeReplace, (unsigned char *) &mwmhints, sizeof (XWMHints) / 4);
 
-	p->display = display;
-	p->window  = window;
-	p->_NET_SUPPORTED = is_has_net_supported(display, root);
+	int _NET_SUPPORTED = is_has_net_supported(display, root);
 
 	log_message(LOG, _("Main window with id %d created"), window);
 
 	XSynchronize(display, TRUE);
 	XFlush(display);
 
-	return TRUE;
-}
+	struct _keymap* keymap = keymap_init(handle, display);
+	if (keymap == NULL) {
+		XCloseDisplay(display);
+		return NULL;
+	}
 
-static void window_uninit(struct _window *p)
-{
-	if (p->keymap != NULL)
-		p->keymap->uninit(p->keymap);
-
-	free(p);
-
-	log_message(DEBUG, _("Window is freed"));
-}
-
-struct _window* window_init(struct _xneur_handle *handle)
-{
 	struct _window *p = (struct _window *) malloc(sizeof(struct _window));
 	memset(p, 0, sizeof(struct _window));
 
-	if (!window_create(p)) {
-		free(p);
-		return NULL;
-	}
-	p->keymap = keymap_init(handle, p->display);
-	if (p->keymap == NULL) {
-		free(p);
-		return NULL;
-	}
+	p->keymap  = keymap;
+	p->display = display;
+	p->window  = window;
+	p->_NET_SUPPORTED = _NET_SUPPORTED;
+
 	// Function mapping
 	p->uninit = window_uninit;
 
