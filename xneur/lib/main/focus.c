5065ea7a4f126c0a454a7a671e748eb037389ed1
Mingun <Alexander_Sergey@mail.ru>
2020-12-30 23:53:02 +0500

    Simplify expressions

--- xneur/lib/main/focus.c
+++ xneur/lib/main/focus.c
@@ -62,7 +62,7 @@ static int focus_get_focus_status(struct _focus *p, int *forced_mode, int *exclu
 	char *new_app_name = NULL;
 
 	// Clear masking on unfocused window
-	//focus_update_grab_events(p, TRUE || p->last_excluded);
+	//focus_update_grab_events(p, TRUE);
 
 	Window new_window;
 	int show_message = TRUE;
@@ -268,7 +268,7 @@ static void focus_update_grab_events(struct _focus *p, int not_grab)
 
 static void focus_click_key(struct _focus *p, KeySym keysym)
 {
-	focus_update_grab_events(p, TRUE || p->last_excluded);
+	focus_update_grab_events(p, TRUE);
 
 	KeyCode keycode = XKeysymToKeycode(main_window->display, keysym);
 
@@ -276,7 +276,7 @@ static void focus_click_key(struct _focus *p, KeySym keysym)
 	XTestFakeKeyEvent(main_window->display, keycode ,FALSE, 0); // key release event
 	XFlush(main_window->display);
 
-	focus_update_grab_events(p, FALSE || p->last_excluded);
+	focus_update_grab_events(p, p->last_excluded);
 }
 
 static void focus_uninit(struct _focus *p)
